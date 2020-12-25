11d17773eec33b0b9598484f72b58fd49df10c28
Yoan Thirion <yoan.thirion@gmail.com>
2020-12-25 09:50:17 +0100

    Clean day1

--- 2020/src/test/kotlin/Day1.kt
+++ 2020/src/test/kotlin/Day1.kt
@@ -3,10 +3,6 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class Day1 : Day(1) {
-    fun <T, U> cartesianProduct(c1: Collection<T>, c2: Collection<U>): List<Pair<T, U>> {
-        return c1.flatMap { lhsElem -> c2.map { rhsElem -> lhsElem to rhsElem } }
-    }
-
     private fun calculateFor2020(lines: List<String>, entries: Int): Int =
             lines.map { it.toInt() }
                     .toVavrList()
