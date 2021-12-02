7f979e9de316e0a52710170abb4f6e4f5cbba71a
Yoan Thirion <yoan.thirion@gmail.com>
2021-12-02 09:51:11 +0400

    add 2021 day 1

---
+++ 2021/src/test/kotlin/Day1.kt
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day1 : Day(1) {
+    private fun `number of times a depth measurement increases`(
+        lines: List<String>,
+        size: Int
+    ): Int =
+        lines.toInts()
+            .windowed(size) { it.sum() }
+            .windowed(2).count { it.last() > it.first() }
+
+    @Test
+    fun exercise1() =
+        Assertions.assertEquals(1393, computeResult { `number of times a depth measurement increases`(it, 1) })
+
+    @Test
+    fun exercise2() =
+        Assertions.assertEquals(1359, computeResult { `number of times a depth measurement increases`(it, 3) })
+}
