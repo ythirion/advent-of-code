21f669b393b3fb7e05b85c6e06397658e71656d7
Yoan Thirion <yoan.thirion@gmail.com>
2021-12-08 06:42:32 +0100

    Add day 8 part1

--- 2021/src/test/kotlin/Day8.kt
+++ 2021/src/test/kotlin/Day8.kt
@@ -5,13 +5,11 @@ class Day8 : Day(8, "Seven Segment Search") {
     private val digitSegments = mapOf(0 to 6, 1 to 2, 2 to 5, 3 to 5, 4 to 4, 5 to 5, 6 to 6, 7 to 3, 8 to 7, 9 to 6)
 
     private fun dirtySolve(input: List<String>): Int {
+        val searchFor = listOf(1, 4, 7, 8).map { digitSegments[it] }
         return input.sumOf { line ->
             line.split("|")[1]
                 .split(" ")
-                .count {
-                    it.length == digitSegments[1] || it.length == digitSegments[4] ||
-                            it.length == digitSegments[7] || it.length == digitSegments[8]
-                }
+                .count { searchFor.contains(it.length) }
         }
     }
 
