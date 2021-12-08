160f96859c131ae14fa6ca78a9c63251c15c0c50
Yoan Thirion <yoan.thirion@gmail.com>
2021-12-08 06:36:27 +0100

    Add day 8 part1

--- 2021/src/test/kotlin/Day8.kt
+++ 2021/src/test/kotlin/Day8.kt
@@ -1,12 +1,23 @@
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
-class Day8 : Day(8, "") {
+class Day8 : Day(8, "Seven Segment Search") {
+    private val digitSegments = mapOf(0 to 6, 1 to 2, 2 to 5, 3 to 5, 4 to 4, 5 to 5, 6 to 6, 7 to 3, 8 to 7, 9 to 6)
+
+    private fun dirtySolve(input: List<String>): Int {
+        return input.sumOf { line ->
+            line.split("|")[1]
+                .split(" ")
+                .count {
+                    it.length == digitSegments[1] || it.length == digitSegments[4] ||
+                            it.length == digitSegments[7] || it.length == digitSegments[8]
+                }
+        }
+    }
 
     @Test
     fun part1() =
         Assertions.assertEquals(
-            331067,
-            computeResult {
-            })
+            349,
+            computeResult { dirtySolve(it) })
 }
