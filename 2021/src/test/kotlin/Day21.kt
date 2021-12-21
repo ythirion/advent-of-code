b9c0ac8af978879305ad3bcb18da3b5a9ed6964e
Yoan Thirion <yoan.thirion@gmail.com>
2021-12-21 15:36:03 +0100

    use Players

--- 2021/src/test/kotlin/Day21.kt
+++ 2021/src/test/kotlin/Day21.kt
@@ -2,26 +2,23 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class Day21 : Day(21, "Dirac Dice") {
+    data class Player(val pawn: Int, val score: Int = 0)
+    data class GameResult(val winner: Player, val loser: Player, val totalRolls: Int)
+
     private val deterministicDice = generateSequence(1) { if (it == 100) 1 else it + 1 }.iterator()
     private val diracDiceCombinations = listOf(1, 2, 3).let {
         it.flatMap { dice1 -> it.flatMap { dice2 -> it.map { dice3 -> Triple(dice1, dice2, dice3) } } }
     }
 
-    private val diracDice = listOf(1, 2, 3).let {
-        it.flatMap { dice1 -> it.flatMap { dice2 -> it.map { dice3 -> dice1 + dice2 + dice3 } } }
-    }
-
-    data class Player(val pawn: Int, val score: Int = 0)
-    data class GameResult(val winner: Player, val loser: Player, val totalRolls: Int)
-
     private fun Player.movePawn(dice: Int): Player =
-        this.copy(pawn = ((pawn + dice) % 10).apply {
-            if (this == 0) this + 10
+        this.copy(pawn = ((pawn + dice) % 10).let {
+            if (it == 0) it + 10 else it
         })
 
     private fun Player.score(): Player = copy(score = this.score + this.pawn)
-    private fun Player.whoWins(other: Player): Player = if (this.score > other.score) this else other
-    private fun List<Player>.endGame(winningScore: Int): Boolean = this.any { it.score >= winningScore }
+    private fun Player.reached(winningScore: Int): Boolean = this.score >= winningScore
+    private fun List<Player>.winner(): Player = if (this[0].score > this[1].score) this[0] else this[1]
+    private fun List<Player>.reached(winningScore: Int): Boolean = this.any { it.reached(winningScore) }
 
     private fun play(
         player1: Player,
@@ -29,11 +26,11 @@ class Day21 : Day(21, "Dirac Dice") {
         winningScore: Int,
         roll: () -> List<Int>
     ): GameResult {
-        var rolls = 0
         val players = mutableListOf(player1, player2)
+        var rolls = 0
         var playerIndex = 0
 
-        while (true) {
+        while (!players.reached(winningScore)) {
             var currentPlayer = players[playerIndex]
             roll().map { dice ->
                 currentPlayer = currentPlayer.movePawn(dice)
@@ -41,48 +38,40 @@ class Day21 : Day(21, "Dirac Dice") {
             }
             players[playerIndex] = currentPlayer.score()
             playerIndex = (playerIndex + 1) % 2
-
-            if (players.endGame(winningScore))
-                break
         }
 
-        players[0].whoWins(players[1])
-            .let { winner ->
-                return GameResult(winner, if (winner == players[0]) players[1] else players[0], rolls)
-            }
+        return players
+            .winner()
+            .let { winner -> GameResult(winner, if (winner == players[0]) players[1] else players[0], rolls) }
     }
 
     private fun playWithDiracDice(
-        position1: Int,
-        position2: Int,
+        player1: Player,
+        player2: Player,
         cache: HashMap<String, Pair<Long, Long>>,
-        score1: Int = 0,
-        score2: Int = 0,
+        winningScore: Int = 21
     ): Pair<Long, Long> {
-        if (score1 >= 21) return Pair(1, 0)
-        if (score2 >= 21) return Pair(0, 1)
+        if (player1.reached(winningScore)) return Pair(1, 0)
+        if (player2.reached(winningScore)) return Pair(0, 1)
 
-        fun key(): String = "$position1,$score1-$position2,$score2"
+        fun key(): String = "$player1-$player2"
 
         if (key() in cache.keys)
             return cache[key()]!!
 
-        var player1 = 0L
-        var player2 = 0L
+        var win1 = 0L
+        var win2 = 0L
 
-        diracDiceCombinations.forEach { diracDice ->
-            val newPosition1 = ((position1 + diracDice.toList().sum()) % 10).let {
-                if (it == 0) it + 10 else it
-            }
-            val newScore1 = score1 + newPosition1
+        diracDiceCombinations.forEach { dice ->
+            val newPlayer1 = player1.movePawn(dice.toList().sum()).score()
+            val inOtherUniverses = playWithDiracDice(player2, newPlayer1, cache)
 
-            val inOtherUniverses = playWithDiracDice(position2, newPosition1, cache, score2, newScore1)
-            cache["$position2,$score2-$newPosition1,$newScore1"] = inOtherUniverses
+            cache["$player2-$newPlayer1"] = inOtherUniverses
 
-            player2 += inOtherUniverses.first
-            player1 += inOtherUniverses.second
+            win2 += inOtherUniverses.first
+            win1 += inOtherUniverses.second
         }
-        return Pair(player1, player2)
+        return Pair(win1, win2)
     }
 
     private fun `what do you get if you multiply the score of the losing player by the number of times the die was rolled during the game`(): Int =
@@ -91,9 +80,9 @@ class Day21 : Day(21, "Dirac Dice") {
         }.let { it.loser.score * it.totalRolls }
 
     private fun `find the player that wins in more universes in how many universes does that player win`() =
-        playWithDiracDice(5, 8, hashMapOf()).let {
-            if (it.first > it.second) it.first else it.second
-        }
+        playWithDiracDice(Player(5), Player(8), hashMapOf())
+            .toList()
+            .maxOf { it }
 
     @Test
     fun part1() =
