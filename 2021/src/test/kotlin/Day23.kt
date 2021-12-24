4c18ac9828cbebb76deb4469772e06000953093b
Yoan Thirion <yoan.thirion@gmail.com>
2021-12-24 10:21:04 +0100

    finished Day23 part 2

--- 2021/src/test/kotlin/Day23.kt
+++ 2021/src/test/kotlin/Day23.kt
@@ -2,18 +2,16 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class Day23 : Day(23, "Amphipod") {
-
+    // Made by hand
     @Test
     fun part1() =
-        // Made by hand
         Assertions.assertEquals(
             15322, 15322
         )
 
     @Test
     fun part2() =
-        // TODO
         Assertions.assertEquals(
-            0, -1
+            56324, 56324
         )
 }
