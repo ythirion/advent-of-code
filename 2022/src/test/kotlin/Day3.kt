c3aea9a303798cf8d486c6501a3a53430ae670c2
Yoan Thirion <yoan.thirion@gmail.com>
2022-12-17 00:20:45 +0100

    refactor day3

--- 2022/src/test/kotlin/Day3.kt
+++ 2022/src/test/kotlin/Day3.kt
@@ -1,8 +1,7 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-private typealias Compartment = String
-private typealias Rucksack = Pair<Compartment, Compartment>
+private typealias Rucksack = String
 private typealias Group = List<Rucksack>
 class Day3 : Day(3, "Rucksack Reorganization") {
     private val priorities = (('a'..'z') + ('A' .. 'Z'))
@@ -10,35 +9,36 @@ class Day3 : Day(3, "Rucksack Reorganization") {
         .toMap()
 
     private fun Rucksack.misplacedItem(): Char =
-        first.toSet()
-            .first { item -> second.contains(item) }
-
-    private fun String.toRucksack(): Rucksack =
-        (this.length / 2).let { half ->
-            Rucksack(substring(0, half), substring(half, length))
+        splitInCompartments().let { compartments ->
+            compartments.first
+            .toSet()
+            .first { item -> compartments.second.contains(item) }
         }
 
-    private fun List<String>.toRucksacks(): List<Rucksack> = map { it.toRucksack() }
+    private fun String.splitInCompartments(): Pair<String, String> =
+        (this.length / 2).let { half ->
+            Pair(substring(0, half), substring(half, length))
+        }
 
     private fun List<Rucksack>.organizeInGroups(): List<Group> = this.chunked(3)
-    private fun Rucksack.allItems(): String = first + second
-
     private fun List<Char>.calculatePriorities(): Int = sumOf { priorities[it]!! }
 
     private fun Group.badge(): Char =
-        this.elementAt(0)
-            .allItems()
-            .first {item ->
-                elementAt(1).allItems().contains(item)
-                        && elementAt(2).allItems().contains(item)
-            }
+        elementAt(0)
+            .keepOnlyItemsAlsoIn(elementAt(1))
+            .keepOnlyItemsAlsoIn(elementAt(2))
+            .single()
+
+    private fun Rucksack.keepOnlyItemsAlsoIn(other: Rucksack): String =
+        this.toSet()
+            .filter { i -> other.contains(i) }
+            .joinToString()
 
     @Test
     fun part1() =
         assertEquals(7568,
             computeResult {
-                it.toRucksacks()
-                    .map { rucksack -> rucksack.misplacedItem() }
+                it.map { rucksack -> rucksack.misplacedItem() }
                     .calculatePriorities()
             }
         )
@@ -47,8 +47,7 @@ class Day3 : Day(3, "Rucksack Reorganization") {
     fun part2() =
         assertEquals(2780,
             computeResult {
-                it.toRucksacks()
-                    .organizeInGroups()
+                it.organizeInGroups()
                     .map { group -> group.badge() }
                     .calculatePriorities()
             }
