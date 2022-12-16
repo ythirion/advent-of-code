ad330ebcc496125476f656bd578a338863bc7383
Yoan Thirion <yoan.thirion@gmail.com>
2022-12-17 00:13:28 +0100

    day 3 part 2

--- 2022/src/test/kotlin/Day3.kt
+++ 2022/src/test/kotlin/Day3.kt
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test
 
 private typealias Compartment = String
 private typealias Rucksack = Pair<Compartment, Compartment>
+private typealias Group = List<Rucksack>
 class Day3 : Day(3, "Rucksack Reorganization") {
     private val priorities = (('a'..'z') + ('A' .. 'Z'))
         .mapIndexed { i, c -> Pair(c, i + 1) }
@@ -10,7 +11,7 @@ class Day3 : Day(3, "Rucksack Reorganization") {
 
     private fun Rucksack.misplacedItem(): Char =
         first.toSet()
-            .find { item -> second.contains(item) }!!
+            .first { item -> second.contains(item) }
 
     private fun String.toRucksack(): Rucksack =
         (this.length / 2).let { half ->
@@ -19,15 +20,36 @@ class Day3 : Day(3, "Rucksack Reorganization") {
 
     private fun List<String>.toRucksacks(): List<Rucksack> = map { it.toRucksack() }
 
-    private fun List<Rucksack>.calculatePriorities(): Int =
-        this.map { it.misplacedItem() }
-            .sumOf { priorities[it]!! }
+    private fun List<Rucksack>.organizeInGroups(): List<Group> = this.chunked(3)
+    private fun Rucksack.allItems(): String = first + second
+
+    private fun List<Char>.calculatePriorities(): Int = sumOf { priorities[it]!! }
+
+    private fun Group.badge(): Char =
+        this.elementAt(0)
+            .allItems()
+            .first {item ->
+                elementAt(1).allItems().contains(item)
+                        && elementAt(2).allItems().contains(item)
+            }
 
     @Test
     fun part1() =
-        assertEquals(157,
+        assertEquals(7568,
             computeResult {
                 it.toRucksacks()
+                    .map { rucksack -> rucksack.misplacedItem() }
+                    .calculatePriorities()
+            }
+        )
+
+    @Test
+    fun part2() =
+        assertEquals(2780,
+            computeResult {
+                it.toRucksacks()
+                    .organizeInGroups()
+                    .map { group -> group.badge() }
                     .calculatePriorities()
             }
         )
