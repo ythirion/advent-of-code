5201506b5314d843e5cafa845b4583e4675db3b4
Yoan Thirion <yoan.thirion@gmail.com>
2022-12-17 17:55:32 +0100

    day 6 prepare for part 2

--- 2022/src/test/kotlin/Day6.kt
+++ 2022/src/test/kotlin/Day6.kt
@@ -2,10 +2,11 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class Day6 : Day(6, "Tuning Trouble") {
-    private fun String.firstStartOfMarker(): String =
-        drop(1)
-            .windowed(4, 1)
-            .first { it.areAllCharDifferent() }
+    private fun String.firstMarker(size: Int) = drop(1)
+        .windowed(size, 1)
+        .first { it.areAllCharDifferent() }
+
+    private fun String.firstStartOfMarker(): String = firstMarker(4)
 
     private fun String.indexOfFirstStartOfMarker(): Int =
         firstStartOfMarker().let {
