00b1733ef84786408f3a5b87131a639dd07783d3
Yoan Thirion <yoan.thirion@gmail.com>
2022-12-19 00:31:15 +0100

    day 8 part 2

--- 2022/src/test/kotlin/Day8.kt
+++ 2022/src/test/kotlin/Day8.kt
@@ -10,6 +10,9 @@ class Day8 : Day(8, "Treetop Tree House") {
     private fun TreeGrid.up(x: Int, y: Int): List<Tree> = take(x).map { it[y] }
     private fun TreeGrid.down(x: Int, y: Int): List<Tree> = drop(x + 1).map { it[y] }
     private fun Tree.isVisible(trees: List<Tree>): Boolean = trees.isEmpty() || trees.all { it < this }
+    private fun Tree.howManyTreesCanBeSeen(trees: List<Tree>): Int =
+        trees.indexOfFirst { tree -> tree >= this }
+            .let { index -> if (index != -1) index + 1 else trees.size }
 
     private fun TreeGrid.isVisible(x: Int, y: Int): Boolean =
         this[x][y].let { tree ->
@@ -19,11 +22,24 @@ class Day8 : Day(8, "Treetop Tree House") {
                     || tree.isVisible(down(x, y))
         }
 
+    private fun TreeGrid.scenicScore(x: Int, y: Int): Int =
+        this[x][y].let { tree ->
+            tree.howManyTreesCanBeSeen(left(x, y).reversed()) *
+                    tree.howManyTreesCanBeSeen(right(x, y)) *
+                    tree.howManyTreesCanBeSeen(up(x, y).reversed()) *
+                    tree.howManyTreesCanBeSeen(down(x, y))
+        }
+
     private fun TreeGrid.whichTreesAreVisible(): List<Tree> =
         flatMapIndexed { x: Int, line: List<Tree> ->
             line.mapIndexedNotNull { y, tree ->
                 if (isVisible(x, y)) tree else null
             }
+        }
+
+    private fun TreeGrid.calculateScenicScores(): List<List<Int>> =
+        mapIndexed { x: Int, line: List<Tree> ->
+            List(line.size) { y -> scenicScore(x, y) }
         }
 
     private fun List<String>.toTreeGrid(): TreeGrid =
@@ -36,6 +52,20 @@ class Day8 : Day(8, "Treetop Tree House") {
             computeResult(
                 { it.toTreeGrid() },
                 { it.whichTreesAreVisible().size }
+            )
+        )
+
+    @Test
+    fun part2() =
+        assertEquals(
+            480000,
+            computeResult(
+                { it.toTreeGrid() },
+                { trees ->
+                    trees.calculateScenicScores()
+                        .flatten()
+                        .maxOf { it }
+                }
             )
         )
 }
