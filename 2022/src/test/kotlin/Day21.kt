b693d924edf0a638b016524498e9f06773e03a2e
Yoan Thirion <yoan.thirion@gmail.com>
2022-12-22 02:32:12 +0100

    day 21 part 2...

--- 2022/src/test/kotlin/Day21.kt
+++ 2022/src/test/kotlin/Day21.kt
@@ -1,48 +1,30 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-typealias Monkeys = List<Monkey>
-typealias Observers = MutableMap<String, MutableList<Mather>>
-
-abstract class Monkey(open val name: String) {
-    open fun canYell(): Boolean = true
-    abstract fun numberToYell(): Long
-    fun yell(observers: Observers) {
-        if (observers.containsKey(name)) {
-            observers[name]!!.forEach { it.monkeyYelled(name, numberToYell()) }
-        }
-    }
+sealed class Monkey(val name: String) {
+    abstract fun evaluate(monkeys: Monkeys): Long
 }
 
-class Yeller(override val name: String, private val number: Long) : Monkey(name) {
-    override fun numberToYell(): Long = number
+class Yeller(name: String, private val value: Long) : Monkey(name) {
+    override fun evaluate(monkeys: Monkeys): Long = value
 }
 
 class Mather(
-    override val name: String,
-    val monkey1: String,
+    name: String,
+    val left: String,
     private val operator: Char,
-    val monkey2: String
+    val right: String
 ) : Monkey(name) {
-    private var value1: Long? = null
-    private var value2: Long? = null
-
-    fun monkeyYelled(monkey: String, number: Long) {
-        if (monkey == monkey1) value1 = number
-        else value2 = number
-    }
-
-    override fun canYell(): Boolean =
-        value1 != null && value2 != null
-
-    override fun numberToYell(): Long =
+    override fun evaluate(monkeys: Monkeys): Long =
         when (operator) {
-            '+' -> value1!! + value2!!
-            '-' -> value1!! - value2!!
-            '*' -> value1!! * value2!!
-            else -> value1!! / value2!!
+            '+' -> monkeys[left]!!.evaluate(monkeys) + monkeys[right]!!.evaluate(monkeys)
+            '-' -> monkeys[left]!!.evaluate(monkeys) - monkeys[right]!!.evaluate(monkeys)
+            '*' -> monkeys[left]!!.evaluate(monkeys) * monkeys[right]!!.evaluate(monkeys)
+            else -> monkeys[left]!!.evaluate(monkeys) / monkeys[right]!!.evaluate(monkeys)
         }
 }
+
+typealias Monkeys = Map<String, Monkey>
 
 class Day21 : Day(21, "Monkey Math") {
     private fun Pair<String, String>.toMonkey(): Monkey =
@@ -54,53 +36,57 @@ class Day21 : Day(21, "Monkey Math") {
         }
 
     private fun List<String>.toMonkeys(): Monkeys =
-        map {
-            it.split(": ").let { words ->
-                Pair(words[0], words[1]).toMonkey()
+        this.associate { line ->
+            val (monkey, expression) = line.split(": ")
+            monkey to Pair(monkey, expression).toMonkey()
+        }
+
+
+    private fun Monkeys.findNumberToPassRootEquality(): Long =
+        (this["root"]!! as Mather).let { root ->
+            val leftMonkey = this[root.left]!!
+            val rightMonkey = this[root.right]!!
+
+            val clone = this.toMutableMap()
+            val rightValue = rightMonkey.evaluate(clone)
+
+            val y1 = 0L
+            clone["humn"] = Yeller("humn", y1)
+            val l1 = leftMonkey.evaluate(clone)
+
+            val y2 = 9_000_000_000L
+            clone["humn"] = Yeller("humn", y2)
+            val l2 = leftMonkey.evaluate(clone)
+
+            val m1 = (y2 - y1).toDouble() / (l2 - l1).toDouble()
+            val b1 = y2 - (m1 * l2)
+            val y = (m1 * rightValue + b1).toLong()
+
+            for (t in y - 1000..y + 1000) {
+                clone["humn"] = Yeller("humn", t)
+                if (leftMonkey.evaluate(clone) == rightValue) return t
             }
+            error("WTF!!!")
         }
-
-    private fun Observers.add(observer: Mather, observed: String) {
-        putIfAbsent(observed, mutableListOf())
-        this[observed]!!.add(observer)
-    }
-
-    private fun Observers.add(monkey: Mather) {
-        add(monkey, monkey.monkey1)
-        add(monkey, monkey.monkey2)
-    }
-
-    private fun Monkeys.toObservers(): Observers =
-        mutableMapOf<String, MutableList<Mather>>().apply {
-            filterIsInstance<Mather>()
-                .forEach { mather -> add(mather) }
-        }
-
-    private fun Monkeys.process(): Long {
-        val observers = toObservers()
-
-        filterIsInstance<Yeller>()
-            .forEach { it.yell(observers) }
-
-        first { it.name == "root" }.let { root ->
-            while (true) {
-                filterIsInstance<Mather>()
-                    .filter { mather -> mather.canYell() }
-                    .forEach { mather -> mather.yell(observers) }
-
-                if (root.canYell())
-                    return root.numberToYell()
-            }
-        }
-    }
 
     @Test
     fun part1() {
         assertEquals(
             66174565793494,
             computeResult {
-                it.toMonkeys()
-                    .process()
+                val expressions = it.toMonkeys()
+                expressions["root"]?.evaluate(expressions)
+            }
+        )
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(
+            3327575724809,
+            computeResult {
+                val expressions = it.toMonkeys()
+                expressions.findNumberToPassRootEquality()
             }
         )
     }
