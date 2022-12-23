47a3589dc5ec7c7095b8801a5fe749c4fe3b6da5
Yoan Thirion <yoan.thirion@gmail.com>
2022-12-23 23:53:02 +0100

    day 23 refacto

--- 2022/src/test/kotlin/Day23.kt
+++ 2022/src/test/kotlin/Day23.kt
@@ -7,18 +7,10 @@ import kotlin.math.abs
 typealias ElvesLocations = List<Point2D>
 
 class Day23 : Day(23, "Unstable Diffusion") {
-    private val elfSymbol = '#'
-    private val emptySymbol = '.'
-
     private enum class Direction(val value: Point2D) {
         N(Point2D(1, 0)), NE(Point2D(1, 1)), NW(Point2D(1, -1)),
         S(Point2D(-1, 0)), SE(Point2D(-1, 1)), SW(Point2D(-1, -1)),
         E(Point2D(0, 1)), W(Point2D(0, -1));
-
-        fun isNorth() = this in listOf(N, NE, NW)
-        fun isSouth() = this in listOf(S, SE, SW)
-        fun isWest() = this in listOf(W, NW, SW)
-        fun isEast() = this in listOf(E, NE, SE)
     }
 
     private fun Point2D.moveTo(direction: Direction): Point2D =
@@ -28,41 +20,40 @@ class Day23 : Day(23, "Unstable Diffusion") {
         mutableListOf<Point2D>().let { elves ->
             reversed().forEachIndexed { x, line ->
                 line.forEachIndexed { y, c ->
-                    if (c == elfSymbol) elves.add(Point2D(x, y))
+                    if (c == '#') elves.add(Point2D(x, y))
                 }
             }
             return elves
         }
 
-    private fun Point2D.whatsAt(direction: Direction, elves: ElvesLocations): Pair<Point2D, Char> =
+    private fun Point2D.whatsAt(direction: Direction, elves: ElvesLocations): Point2D? =
         moveTo(direction).let { point ->
-            return Pair(point, if (elves.firstOrNull { it == point } != null) elfSymbol else emptySymbol)
+            return if (elves.firstOrNull { it == point } != null) point else null
         }
 
-    private fun Point2D.adjacent(elves: ElvesLocations): Map<Direction, Pair<Point2D, Char>> =
-        values().associateWith { direction ->
-            whatsAt(direction, elves)
-        }
+    private fun Point2D.adjacentElves(elves: ElvesLocations): Map<Direction, Point2D> =
+        Direction.values()
+            .associateWith { direction -> whatsAt(direction, elves) }
+            .filterNot { it.value == null }
+            .mapValues { it.value!! }
 
-    private fun Map<Direction, Pair<Point2D, Char>>.containsElves(): Boolean = values.any { it.second == elfSymbol }
-
-    private fun Map.Entry<Point2D, Map<Direction, Pair<Point2D, Char>>>.propose(direction: Direction): Point2D? =
+    private fun Map.Entry<Point2D, Map<Direction, Point2D>>.propose(direction: Direction): Point2D? =
         if (!containsElves(direction)) key.moveTo(direction) else null
 
-    private fun Map.Entry<Point2D, Map<Direction, Pair<Point2D, Char>>>.proposeFirstValidDirection(strategy: Queue<Direction>): Point2D? =
+    private fun Map.Entry<Point2D, Map<Direction, Point2D>>.proposeFirstValidDirection(strategy: Queue<Direction>): Point2D? =
         strategy.toList()
             .map { direction -> this.propose(direction) }
             .firstNotNullOfOrNull { it }
 
-    private fun Map.Entry<Point2D, Map<Direction, Pair<Point2D, Char>>>.containsElves(direction: Direction) =
+    private fun Map.Entry<Point2D, Map<Direction, Point2D>>.containsElves(direction: Direction) =
         value.filterKeys {
             when (direction) {
-                N -> it.isNorth()
-                S -> it.isSouth()
-                W -> it.isWest()
-                else -> it.isEast()
+                N -> it in listOf(N, NE, NW)
+                S -> it in listOf(S, SE, SW)
+                W -> it in listOf(W, NW, SW)
+                else -> it in listOf(E, NE, SE)
             }
-        }.containsElves()
+        }.any()
 
     private fun ElvesLocations.moveElves(newElvesLocations: Map<Point2D, Point2D>): ElvesLocations =
         toMutableList().let { newElves ->
@@ -78,53 +69,40 @@ class Day23 : Day(23, "Unstable Diffusion") {
             return strategy
         }
 
-    private fun ElvesLocations.elvesToMove(strategy: Queue<Direction>): Map<Point2D, Point2D> {
-        this.associateWith { elf -> elf.adjacent(this) }
-            .filter { it.value.containsElves() }.let { elves ->
-                val proposal = elves
-                    .map { elf -> elf.key to elf.proposeFirstValidDirection(strategy) }
+    private fun ElvesLocations.elvesToMove(strategy: Queue<Direction>): Map<Point2D, Point2D> =
+        this.associateWith { elf -> elf.adjacentElves(this) }
+            .filter { it.value.any() }
+            .let { elves ->
+                elves.map { elf -> elf.key to elf.proposeFirstValidDirection(strategy) }
                     .toMap()
-
-                return proposal
-                    .filter { p -> p.value != null && proposal.count { it.value == p.value } == 1 }
-                    .mapValues { v -> v.value!! }
+                    .let { proposal ->
+                        return proposal
+                            .filter { p -> p.value != null && proposal.count { it.value == p.value } == 1 }
+                            .mapValues { v -> v.value!! }
+                    }
             }
-    }
 
-    private fun ElvesLocations.round(times: Int): ElvesLocations {
-        val strategy = LinkedList(listOf(N, S, W, E))
-        var elves = this.toList()
+    private fun ElvesLocations.round(stop: (times: Int, elvesToMove: Int) -> Boolean): Result =
+        LinkedList(listOf(N, S, W, E)).let { strategy ->
+            var elves = this
+            var round = 0
 
-        repeat(times) {
-            val toMove = elves.elvesToMove(strategy)
-            elves = elves.moveElves(toMove)
-            strategy.updateStrategy()
+            do {
+                val toMove = elves.elvesToMove(strategy)
+                elves = elves.moveElves(toMove)
+                strategy.updateStrategy()
+
+                round++
+            } while (!stop(round, toMove.size))
+
+            return Result(round, elves)
         }
-        return elves
-    }
 
-    private fun ElvesLocations.moveElvesUntilNoOneMove(): Int {
-        val strategy = LinkedList(listOf(N, S, W, E))
-        var elves = this.toList()
-        var count = 0
-
-        while (true) {
-            val toMove = elves.elvesToMove(strategy)
-            elves = elves.moveElves(toMove)
-            strategy.updateStrategy()
-            count++
-
-            println("To move = ${toMove.size}")
-
-            if (toMove.isEmpty())
-                return count
-        }
-    }
+    private data class Result(val round: Int, val elves: ElvesLocations)
 
     private fun ElvesLocations.emptyTiles(): Int =
         (maxBy { it.x }.x + 1 + abs(minBy { it.x }.x)) *
-                (maxBy { it.y }.y + 1 + abs(minBy { it.y }.y)) -
-                this.count()
+                (maxBy { it.y }.y + 1 + abs(minBy { it.y }.y)) - count()
 
     @Test
     fun part1() {
@@ -132,7 +110,8 @@ class Day23 : Day(23, "Unstable Diffusion") {
             4146,
             computeResult {
                 it.toElves()
-                    .round(10)
+                    .round { times, _ -> times == 10 }
+                    .elves
                     .emptyTiles()
             }
         )
@@ -144,7 +123,8 @@ class Day23 : Day(23, "Unstable Diffusion") {
             957,
             computeResult {
                 it.toElves()
-                    .moveElvesUntilNoOneMove()
+                    .round { _, elvesToMove -> elvesToMove == 0 }
+                    .round
             }
         )
     }
